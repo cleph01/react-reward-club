d60b0fcdf7cfc4046d98d6c65a947cf7c94070d8
Charlie <charles.j.montoya@gmail.com>
2021-09-08 19:57:25 -0400

    home page set up

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,22 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase from "../firebase/firebase";
 
-import firebaseApp from "../firebase/firebase";
-
 let uiConfig = {
     signInFlow: "popup",
-    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+        signInSuccessWithAuthResult: (user) => {
+            localStorage.setItem("user", user);
+            console.log("User in Config CB: ", user);
+        },
+    },
 };
 
 function Auth() {
+    const [user, setUser] = useState();
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            setIsSignedIn(!isSignedIn);
+            setUser(user);
+        });
+
+        console.log("Did Mount Listener");
+    }, [user]);
+
+    const handleSignOut = (e) => {
+        e.preventDefault();
+
+        // setIsSignedIn(!isSignedIn);
+
+        firebase.auth().signOut();
+    };
+
+    console.log("User from State: ", user);
+    console.log("isSignedIn: ", isSignedIn);
+
     return (
         <>
-            <div>Signup</div>
-            <StyledFirebaseAuth
-                uiConfig={uiConfig}
-                firebaseAuth={firebase.auth()}
-            />
+            {isSignedIn ? (
+                <div>
+                    <div>Signed In!</div>
+                    <button onClick={handleSignOut}>Sign Out</button>
+                </div>
+            ) : (
+                <StyledFirebaseAuth
+                    uiConfig={uiConfig}
+                    firebaseAuth={firebase.auth()}
+                />
+            )}
         </>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,54 @@
 import React from "react";
 
+import { useHistory } from "react-router-dom";
+
+import "../styles/home.scss";
+
+import logo from "../assets/images/logos/logo.png";
+import logo_text from "../assets/images/logos/logo_white_text.png";
+
 function Home() {
-    return <div>Home Page</div>;
+    const history = useHistory();
+
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="menu-wrapper">
+                    <ul className="menu">
+                        <li>Why?</li>
+                        <li>FAQ</li>
+                        <li>Testimonials</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="body-wrapper">
+                <div className="image-wrapper">
+                    <img src={logo} alt="logo" />
+                </div>
+
+                <h3>Win Prizes, Trade, and Get Paid</h3>
+                <p>
+                    Socialiite's mission is to re-define how we use social
+                    media. User's get paid for creating content while also
+                    promoting local businesses.
+                </p>
+                <div className="auth-buttons">
+                    <div
+                        className="buttons"
+                        onClick={() => history.push("/login")}
+                    >
+                        Login
+                    </div>
+                    <div
+                        className="buttons"
+                        onClick={() => history.push("/signup")}
+                    >
+                        Signup
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
